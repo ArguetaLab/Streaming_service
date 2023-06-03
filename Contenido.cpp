13a0f1e0eeb5e636cffcd70f22a1167ec2b0b935
Argueta Lab <81782750+ArguetaLab@users.noreply.github.com>
2023-06-02 21:06:31 -0600

    Create Contenido.cpp

---
+++ Contenido.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "Contenido.h"
+
+using namespace std;
+
+Contenido::Contenido(){
+    name = "";
+    genre = "";
+    rating = 0;
+    ID = "";
+    lenght = 0;
+}
+
+Contenido::Contenido(string _name, string _genre, int _rating, string _ID, float _lenght){
+    name = _name;
+    genre = _genre;
+    rating = _rating;
+    ID = _ID;
+    lenght = _lenght;
+}
+string Contenido::getGenre(){
+return genre;
+}
+string Contenido::getID(){
+return ID;
+}
+float Contenido::getLenght(){
+return lenght;
+}
+string Contenido::getName(){
+return name;
+}
+int Contenido::getRating(){
+return rating;
+}
