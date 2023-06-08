e9e528731503e447f903c5e72492eb51e41b26f2
Argueta Lab <81782750+ArguetaLab@users.noreply.github.com>
2023-06-08 15:33:35 -0600

    Update Pelicula.h

--- Pelicula.h
+++ Pelicula.h
@@ -1,14 +1,13 @@
-#ifndef PELICULA_H_INCLUDED
-#define PELICULA_H_INCLUDED
+#ifndef VIDEOJUEGO_H_INCLUDED
+#define VIDEOJUEGO_H_INCLUDED
 #include "Contenido.h"
 
-class Pelicula: public Contenido{
+class VideoJuego:public Contenido{
 private:
 public:
-    Pelicula(string,string,int,string,float);
-    string muestraContenido()override;
-    string reproducir()override;
-
+    VideoJuego(string,string,int,string,float);
+    string muestraContenido() override;
+    string reproducir() override;
 };
 
-#endif // PELICULA_H_INCLUDED
+#endif // VIDEOJUEGO_H_INCLUDED
