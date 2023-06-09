3d3681b06e608fdefcd25a14d03fb1d777b41aeb
Argueta Lab <81782750+ArguetaLab@users.noreply.github.com>
2023-06-08 21:43:51 -0600

    Update VideoJuego.h

--- VideoJuego.h
+++ VideoJuego.h
@@ -2,9 +2,12 @@
 #define VIDEOJUEGO_H_INCLUDED
 #include "Contenido.h"
 
+// En este apartado estamos creando la clase VideoJuego, la cual no tendrá atributos ya que todos los hereda al igual que sus métodos
+
 class VideoJuego:public Contenido{
 private:
 public:
+    VideoJuego();
     VideoJuego(string,string,int,string,float);
     string muestraContenido() override;
     string reproducir() override;
