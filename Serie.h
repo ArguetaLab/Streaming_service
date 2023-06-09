0fd882eca0e25bcaa3d0eda4691ea4c2b2d30fec
Argueta Lab <81782750+ArguetaLab@users.noreply.github.com>
2023-06-08 21:41:05 -0600

    Update Serie.h

--- Serie.h
+++ Serie.h
@@ -2,16 +2,22 @@
 #define SERIE_H_INCLUDED
 #include "Contenido.h"
 
+/*
+Esta clase va a representar las Series.
+En este caso solo tiene un atributo porque todos los demás se los hereda la clase padre.
+El único atributo que posee la clase como tal, es un vector llamado EP
+Mientras que los métodos 2 son los que heredamos de métodos y el de getCapitulos() nos retorna el vector
+*/
+
 class Serie:public Contenido{
+    private:
+        vector<Capitulo> EP;
 
-private:
-    vector<Capitulo> EP;
-
-public:
-   Serie(string,string,int, string,float, vector<Capitulo> episodios);
-   vector<Capitulo> getCapitulos();
-   string muestraContenido() override;
-   string reproducir() override;
+    public:
+       Serie(string,string,int, string,float, vector<Capitulo> episodios);
+       vector<Capitulo> getCapitulos();
+       string muestraContenido() override;
+       string reproducir() override;
 };
 
 #endif // SERIE_H_INCLUDED
