644b684ee98f7de295570f580377543a785e2cd3
Argueta Lab <81782750+ArguetaLab@users.noreply.github.com>
2023-06-08 21:39:58 -0600

    Update Pelicula.h

--- Pelicula.h
+++ Pelicula.h
@@ -2,12 +2,18 @@
 #define PELICULA_H_INCLUDED
 #include "Contenido.h"
 
+/*
+Esta clase va a representar las Películas.
+En este caso no tiene atributos porque todos se los hereda la clase padre
+mientras que los métodos son también los que heredamos de métodos
+*/
+
 class Pelicula: public Contenido{
-private:
-public:
-    Pelicula(string,string,int,string,float);
-    string muestraContenido()override;
-    string reproducir()override;
+    private:
+    public:
+        Pelicula(string,string,int,string,float);
+        string muestraContenido()override;
+        string reproducir()override;
 
 };
 
