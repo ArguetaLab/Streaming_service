7ff8262db06929b8aa9a9a835d80fb1ad0ee47d6
Argueta Lab <81782750+ArguetaLab@users.noreply.github.com>
2023-06-08 21:36:10 -0600

    Update Capitulo.h

--- Capitulo.h
+++ Capitulo.h
@@ -1,16 +1,27 @@
 #ifndef CAPITULO_H_INCLUDED
 #define CAPITULO_H_INCLUDED
 
-class Capitulo{
-private:
-    float lenghtCap;
-    string nameCap;
-public:
-    Capitulo();
-    Capitulo(string,float);
+/*
+En esta clase, vamos a poner los atributos y los métodos a usar.
+*/
 
-    string reproducir();
-    string getNombre();
+class Capitulo{
+    private:
+        float lenghtCap;
+        string nameCap;
+    public:
+
+        /*
+        Primero creamos el constructor vacío y posteriormente el constructor con 2 variables que va a tomar en cuenta
+        Luego de crear el constructor vamos a reescribir el método reproducir() el cual pertenece a nuestra clase abstracta Contenido
+        El método getNombre nos regresará el nombre del objeto capitulo
+        */
+
+        Capitulo();
+        Capitulo(string,float);
+
+        string reproducir();
+        string getNombre();
 };
 
 #endif // CAPITULO_H_INCLUDED
