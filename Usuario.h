05384cd841e8471b8ba7f6820e8b76aed64d2cec
Argueta Lab <81782750+ArguetaLab@users.noreply.github.com>
2023-06-08 21:43:01 -0600

    Update Usuario.h

--- Usuario.h
+++ Usuario.h
@@ -1,22 +1,35 @@
 #ifndef USUARIO_H_INCLUDED
 #define USUARIO_H_INCLUDED
 
-class Usuario{
-private:
-    string nombre_U;
-    string pin;
-    string tipo_cuenta;
-    string track;
-    fstream archivo;
+/*
+La clase usuario va a ser independiente del resto de clases que tenemos.
+Sus atributos van a ser:
+    + nombre de usuario
+    + contraseña
+    + tipo de cuenta
+    + track (la cual va a ser un string compuesto por las 3 variables anteriores)
+    + declaramos un objeto archivo
 
-public:
-    Usuario();
-    Usuario(string,string,string,string);
-    void tipoDeCuenta(string);
-    void inicioSesion(string);
-    void contrasena(string);
-    void creaTracker();
-    string buscarTracker();
+En los métodos vamos a crear el constructor vacío y el constructor con parámetros.
+Posteriormente inicializamos el resto de métodos.
+*/
+
+class Usuario{
+    private:
+        string nombre_U;
+        string pin;
+        string tipo_cuenta;
+        string track;
+        fstream archivo;
+
+    public:
+        Usuario();
+        Usuario(string,string,string,string);
+        void tipoDeCuenta(string);
+        void inicioSesion(string);
+        void contrasena(string);
+        void creaTracker();
+        string buscarTracker();
 };
 
 
