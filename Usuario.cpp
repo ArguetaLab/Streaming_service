700e8fcb51d137fb909c73fbd8cba22914a88669
Argueta Lab <81782750+ArguetaLab@users.noreply.github.com>
2023-06-08 21:42:18 -0600

    Update Usuario.cpp

--- Usuario.cpp
+++ Usuario.cpp
@@ -9,12 +9,14 @@ Usuario::Usuario(){
 }
 
 Usuario::Usuario(string _nombre_U, string _pin, string _tipo_cuenta,string _track){
-nombre_U = _nombre_U;
-pin = _pin;
-tipo_cuenta = _tipo_cuenta;
-track = _track;
-
+    nombre_U = _nombre_U;
+    pin = _pin;
+    tipo_cuenta = _tipo_cuenta;
+    track = _track;
 }
+
+// En este apartado vamos a inicializar nuestros atributos de nombre, contraseña y tipo de cuenta
+
 void Usuario::inicioSesion(string i){
     nombre_U = i;
 }
@@ -27,9 +29,19 @@ void Usuario::tipoDeCuenta(string i){
     tipo_cuenta = i;
 }
 
+// En este apartado crearemos el track que nos ayudará a buscar por todo el archivo de texto, nuestra cuenta
+// La razón por la que se hace en modo de frase el método de búsqueda, es para facilitarle al programa la tarea de buscar nuestra cuenta, dato por dato
+
 void Usuario::creaTracker(){
     track = nombre_U+"/"+pin+"/"+tipo_cuenta;
 }
+
+/*
+En este método, vamos a crear 2 tipos de archivo, uno va a ser en modo lectura y otro en modo escritura.
+En el while lo que va a ser es buscar linea por linea el track:
+    + si lo encuentra mandará un mensaje de que la cuenta ha sido encontrada
+    + si no la encuentra, mandará un mensaje y creará la cuenta
+*/
 
 string Usuario::buscarTracker(){
     creaTracker();
